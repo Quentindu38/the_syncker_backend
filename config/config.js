f313426766f4b9f3b2045cdf92239bdbcd912874
Christian Barnabé <62568487+christian-barnabe-chabi@users.noreply.github.com>
2021-08-17 14:38:16 +0000

    "speed improved"

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
    * this is the last destination directory of uploaded files
    * it could be any folder
    */
-  syncedDir: "C:/Users/Christian/Desktop/Sync",
+  syncedDir: "./sync",
   // syncedDir: "./sync",
   /**
    * this is used by multer to temporarily save to file in the request
@@ -11,9 +11,6 @@ const config = {
    */
   tmpFolder: "./upload",
 
-  username: "IntegrityInflow",
-  password: "3x676TVX%*]&v38",
-  uniqueId: "34acb9c4-77c5-4c35-9694-0522eb6c7cf6",
   privateKey: "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAK3YGeJEZLl71tKvFlkDHNsEFuoB4wHzQa9QFkP3AvZAdI6GD2+j57aeW+CPu5AbwSOWOjDF1bqpctIUzlJX4Y9UkEpDQIL8XhcrO+QG6I4NmF/tNnNQcAkMjhgV+adc7RM1d+iKW5CYdSj8NocY0m3udhaJLOhqyyT5MCYWzxiFAgMBAAECgYEAmhMBPcyoCMx9JNUQ1Wsw09vYAyYQ79WMzbgJQ8tU0LNnmjmuzJ9T6kKH44sADUTL9KJciIWOW5V6sj7Gbax9t3iABvhgg96M93w4YsrzwYx8QvxoMsYlL82i6nhSMo/u8QVzPt1nca5zpJzwB0JEJLYoyiqQceBNJ/H3qciwpAECQQDY80A6wV+lOQ2P/zQESD1Z5K/xy0KGUfgcPZJ34KFzFnSKCtZZZS0DNVXgXO4gDlPkZF2dsdFD3QwgqYeKzdhBAkEAzSKWB0PLrB2oy/vMFF44Mhkzg47ydVq0GzIKMiGCBEa0cMVk6BtKt4P3ooUdjdzOKzlVr3nEKoNduoog86gPRQJAF5brhvf7xGVduiiPqV+JfDrHfs0RILXM+pUfVfWQ2Ii6h0ACapn7uPuVqN2x7r5gFNds7nalQHHKncqROGCFgQJBAIV85zJNDMGw/0HWVgQ/F8SKH7qRj/eMEb3Mv1PF+Ex+Ejd8TGC6OdHX/5JzmzrQoB0Cj17EF0IvABQ+hJjqGR0CQE80xhGFnJpyB354CFftQSWN7hHM/DeVe0GrVuuqfKUVVk513ohdRnIF5MSKicnSXqTryXs18RlmyzGXXV+kq0M=",
 };
 
